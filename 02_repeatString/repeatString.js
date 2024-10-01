de5b98e5962380182c87236c6e26b9c922601f9e
akshayamaram <akshayamaramjesus@gmail.com>
2024-10-01 11:21:43 +0530

    add: 02_repeatString solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  if (num < 0) return "ERROR";
+  let repeatedString = '';
+  for(let i=0; i<num; i++) {
+    repeatedString = repeatedString+str
+    // console.log(repeatedString)
+  }
+  return repeatedString
 };
 
 // Do not edit below this line
